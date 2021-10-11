69cf38caee10f8493d764dd5458bb0377b1f9429
hxxzxx <89375462+hxxzxx@users.noreply.github.com>
2021-10-11 14:15:26 +0900

    Create ex05.c

---
+++ c00/ex05.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heejlee <heejlee@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 11:52:30 by heejlee           #+#    #+#             */
+/*   Updated: 2021/10/11 14:14:09 by heejlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+void	ft_putchar(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	if (a != '7' || b != '8' || c != '9')
+	{
+		write(1, ",", 1);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
