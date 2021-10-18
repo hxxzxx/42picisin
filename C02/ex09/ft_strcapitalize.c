e19ef4dd32a68a4c9b503d3663adde7958daad02
hxxzxx <89375462+hxxzxx@users.noreply.github.com>
2021-10-18 16:59:49 +0900

    Update ft_strcapitalize.c

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: heejlee <heejlee@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:46:40 by heejlee           #+#    #+#             */
-/*   Updated: 2021/10/18 14:16:12 by heejlee          ###   ########.fr       */
+/*   Updated: 2021/10/18 16:55:15 by heejlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 char	*ft_strcapitalize(char *str)
@@ -15,23 +15,21 @@ char	*ft_strcapitalize(char *str)
 
 	i = 1;
 	if (str[0] >= 97 && str[0] <= 122)
-	{
 		str[0] = str[0] - 32;
-	}
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 65  && str[i] <= 90)
-		{
+		if (str[i] >= 65 && str[i] <= 90)
 			str[i] = str[i] + 32;
-		}
-	if (((str[i - 1] < 65 || str[i - 1] > 90) && (str[i - 1] < 97 || str[i - 1] > 122)) && (str[i - 1] < 48 || str[i - 1] > 57))
+		if ((str[i - 1] < 65 || str[i - 1] > 90)
+			&& (str[i - 1] < 97 || str[i - 1] > 122)
+			&& (str[i - 1] < 48 || str[i - 1] > 57))
 		{
 			if (str[i] >= 97 && str[i] <= 122)
 			{
 				str[i] = str[i] - 32;
 			}
 		}
-	i++;
+		i++;
 	}
 	return (str);
 }
