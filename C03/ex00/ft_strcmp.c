7e2e053a5cefcbd990f1a8ea2436483e488794c6
hxxzxx <89375462+hxxzxx@users.noreply.github.com>
2021-10-23 09:26:45 +0900

    Update ft_strcmp.c

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: heejlee <heejlee@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 19:54:49 by heejlee           #+#    #+#             */
-/*   Updated: 2021/10/18 20:21:33 by heejlee          ###   ########.fr       */
+/*   Updated: 2021/10/23 09:26:06 by heejlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 int	ft_strcmp(char *s1, char *s2)
@@ -17,9 +17,9 @@ int	ft_strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
-			return (1);
+			return (s1[i] - s2[i]);
 		else if (s1[i] < s2[i])
-			return (-1);
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
