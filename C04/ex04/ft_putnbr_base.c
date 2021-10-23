f2e7ed84d34cd691755579408f208fbbf0dfc435
hxxzxx <89375462+hxxzxx@users.noreply.github.com>
2021-10-23 12:47:55 +0900

    Update ft_putnbr_base.c

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -1,50 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heejlee <heejlee@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 12:39:49 by heejlee           #+#    #+#             */
+/*   Updated: 2021/10/23 12:47:10 by heejlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <unistd.h>
-voidft_putchar(char c)
+void	ft_putchar(char c)
 {
-write(1,&c,1);
-}
-voidnbase(int nbr, int i, char *base)
-{
-char *a;
-int j;
-
-j = 0;
-a = NULL;
-while (nbr != 0)
-{
-nbr = nbr / i;
-a[j] = base[nbr % i];
-j++;
-}
-while (j >= 0)
-{
-ft_putchar(a[j]);
-j--;
-}
+	write(1, &c, 1);
 }
 
-intexpbase(int i, char *base)
+void	nbase(int nbr, int i, char *base)
 {
-if (i == 1 || i == 0
-|| base[i] == base[i+1] 
-|| base[i] == '+' || base[i] == '-')
-return (0);
-return (1);
-}
-voidft_putnbr_base(int nbr, char *base)
-{
-inti;
-inta;
+	char	a[100];
+	int		j;
 
-i = 0;
-if (nbr < 0)
-{
-nbr = nbr * (-1);
-ft_putchar('-');
+	j = 0;
+	while (nbr != 0)
+	{
+		a[j] = base[nbr % i];
+		nbr = nbr / i;
+		j++;
+	}
+	while (j--)
+	{
+		ft_putchar(a[j]);
+	}
 }
-while (base[i] != '\0')
-i++;
-a = expbase(i, base);
-if (a == 1)
-nbase(nbr, i, base);
+
+int	expbase(int i, char *base)
+{
+	int	j;
+
+	j = 0;
+	if (i == 1 || i == 0
+		|| base[j] == base[j + 1]
+		|| base[j] == '+' || base[j] == '-')
+	{
+		j++;
+		return (0);
+	}
+	else
+		return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	i;
+	int	a;
+
+	i = 0;
+	if (nbr < 0)
+	{
+		nbr = nbr * (-1);
+		ft_putchar('-');
+	}
+	while (base[i] != '\0')
+		i++;
+	a = expbase(i, base);
+	if (a == 1)
+		nbase(nbr, i, base);
 }
