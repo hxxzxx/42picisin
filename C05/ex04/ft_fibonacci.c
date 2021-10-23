106b6d9ad5e57f73f6b76fbdf63e7bd3764561dc
hxxzxx <89375462+hxxzxx@users.noreply.github.com>
2021-10-23 13:55:27 +0900

    Create ft_fibonacci.c

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heejlee <heejlee@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 13:47:06 by heejlee           #+#    #+#             */
+/*   Updated: 2021/10/23 13:53:26 by heejlee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	else if (index <= 1)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
