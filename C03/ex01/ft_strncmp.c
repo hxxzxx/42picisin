e372bf01596fa9c3fedeb3b7e243f373e15f4b24
hxxzxx <89375462+hxxzxx@users.noreply.github.com>
2021-10-25 10:24:42 +0900

    Update ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,23 +6,17 @@
 /*   By: heejlee <heejlee@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 20:24:36 by heejlee           #+#    #+#             */
-/*   Updated: 2021/10/23 09:27:38 by heejlee          ###   ########.fr       */
+/*   Updated: 2021/10/25 10:21:36 by heejlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
-	int	s;
+	unsigned int	i;
 
 	i = 0;
-	s = n;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < s)
-	{
-		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
+	if (n == 0)
+		return (0);
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i + 1 < n)
 		i++;
-	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
