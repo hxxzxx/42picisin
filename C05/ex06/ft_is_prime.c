f7b42619e4728b997a58062c188df90448cc738c
hxxzxx <89375462+hxxzxx@users.noreply.github.com>
2021-10-26 17:05:55 +0900

    Update ft_is_prime.c

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -1,17 +1,17 @@
-12 int ft_is_prime(int nb)
- 13 {
- 14     int i;
- 15 
- 16     i = 2;
- 17     if (nb <= 1)
- 18         return (0);
- 19     else if (nb == 2)
- 20         return (1);
- 21     while (i < nb / 2)
- 22     {
- 23         if (i % nb == 0)
- 24     return (0);
- 25         i++;
- 26     }
- 27     return (1);
- 28 }
+int ft_is_prime(int nb)
+{
+    int i;
+
+i = 2;
+    if (nb <= 1)
+return (0);
+    else if (nb == 2)
+return (1);
+    while (i < nb / 2)
+    {
+if (nb % i == 0)
+return (0);
+i++;
+    }
+    return (1);
+}
